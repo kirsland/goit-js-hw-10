260b50bb5d026e5be5c165d09b35f4f39761b04e
kirsland <116314251+kirsland@users.noreply.github.com>
2023-04-24 11:44:40 +0300

    2023-04-24-hw-10-fix1

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const DEBOUNCE_DELAY = 300;
 const searchBox = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
-searchQuery = '';
+let searchQuery = '';
 
 searchBox.addEventListener(
   'input',
